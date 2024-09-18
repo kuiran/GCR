58fb218f058d48843de4150dfb68d998ba725666
王岿然 <wangkuiran@wangkuirandeMacBook-Air.local>
2024-09-18 18:56:12 +0800

    commit first

--- .history/mmtrack/models/gcr/__init___20240918162901.py
+++
@@ -1,8 +0,0 @@
-from .gcr import SinglePosAnchorRPNHead, UnifiedTrackersInputRoIHead, UTIDIIIOUHead, UTIDIIBoxHead, AttentionHead1, GCR
-from .refine_head import UTIDIIroiIOUHead, UTIDIIroiBoxHead
-from .gcr_head import GuidedConv
-
-__all__ = ['SinglePosAnchorRPNHead', 'UnifiedTrackersInput', 'UTIDIIHead', 
-            'UTIDIIIOUHead', 'UTIDIIBoxHead', 'UnifiedTrackersInputRoIHead',
-           'UnifiedTrackersInput1', 'AttentionHead1', 'UnifiedTrackersInput2',
-           'UTIDIIroiIOUHead', 'UTIDIIroiBoxHead', 'GuidedConv']
--- .history/mmtrack/models/gcr/__init___20240918165300.py
+++
@@ -1,5 +0,0 @@
-from .gcr import GCR, SinglePosAnchorRPNHead
-from .gcr_head import GuidedConv
-from .gcr_head import GCR_head, GCRIOUHead, GCRBoxHead
-
-__all__ = ['SinglePosAnchorRPNHead', 'GCR', 'GuidedConv', 'GCR_head', 'GCRIOUHead', 'GCRBoxHead']
